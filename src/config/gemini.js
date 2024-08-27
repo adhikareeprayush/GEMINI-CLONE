8a1c0438e245c841ae893a1c109cbcc62b4d8bb7
Prayush Adhikari <115662438+adhikareeprayush@users.noreply.github.com>
2024-08-27 16:32:36 +0545

    Update gemini.js

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -31,7 +31,6 @@ async function run(prompt) {
     });
 
     const result = await chatSession.sendMessage(prompt);
-    console.log(result.response.text());
     return result.response.text();
 }
 
